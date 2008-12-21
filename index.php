bc9399ad2e8a7c878ab71cd5e7cd5342174f353a
Andy Macdonald <andy@sofawarmer.com>
2008-12-21 12:17:37 -0500

    files from svn

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<html>
+<head>
+
+	<title>page title here</title>
+
+	<!-- stylesheets -->
+		<link rel="stylesheet" href="http://yui.yahooapis.com/2.6.0/build/reset/reset-min.css" type="text/css" charset="utf-8" />
+		<link rel="stylesheet" href="assets/css/style.css" type="text/css" charset="utf-8" />
+
+	<!-- javascript -->
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js" charset="utf-8"></script>
+		
+	<!-- Unit PNG Fix -->
+		<!--[if lt IE 7]>
+        	<script type="text/javascript" src="assets/js/unitpngfix.js"></script>
+		<![endif]-->
+
+</head>
+<body>
+
+page body here
+
+</body>
+</html>
